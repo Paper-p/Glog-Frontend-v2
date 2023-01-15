67f2b2efd92321d6f91740f05b27cd7e8cb13c49
jongjin <jongjin024@gmail.com>
2023-01-15 16:56:23 +0900

    fix: description

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,6 @@ function Head() {
     <>
       <title>Glog</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='서로 성장해나가는 게시판' />
       <link rel='icon' href='/favicon.ico' />
     </>
   );
