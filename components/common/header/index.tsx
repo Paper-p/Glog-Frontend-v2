67abbcb42250d5e2e4e3e7b160eae08d25084713
jongjin <jongjin024@gmail.com>
2023-01-15 18:33:26 +0900

    add: HeaderLayout

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Logo from '../logo';
 import * as S from './style';
 
 function Header() {
-  return <S.HeaderLayout>HEADER</S.HeaderLayout>;
+  const [logged, _] = useState<boolean>(true);
+
+  return (
+    <S.HeaderLayout>
+      <S.Elements className='left-part'>
+        <Logo width={90} height={25} />
+        <S.Box>
+          <p>홈</p>
+          <p>게시물 작성</p>
+        </S.Box>
+      </S.Elements>
+      <S.Elements className='right-part'>
+        {logged ? (
+          <React.Fragment>
+            <p>오종진님</p>
+            <S.UserProfileImage src='https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt361d06c667f47dac/60dad6580401cb0ebfa74b50/dbf90410e8ce567dd5ea7b1d2e6972627d587f4b.jpg' />
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            <p>로그인</p>
+            <p>회원가입</p>
+          </React.Fragment>
+        )}
+      </S.Elements>
+    </S.HeaderLayout>
+  );
 }
 
 export default Header;
--- components/common/header/style.ts
+++ components/common/header/style.ts
@@ -1,3 +1,50 @@
 import styled from '@emotion/styled';
 
-export const HeaderLayout = styled.section``;
+export const HeaderLayout = styled.section`
+  display: flex;
+  width: 72.5%;
+  margin: 25px auto 0;
+  justify-content: space-between;
+  height: 70px;
+`;
+
+export const Elements = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 8px;
+
+  &.right-part {
+    gap: 16px;
+
+    p {
+      font-family: 'Pretendard';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 17px;
+      color: #e0e0e0;
+    }
+  }
+`;
+
+export const UserProfileImage = styled.img`
+  width: 35px;
+  height: 35px;
+  border-radius: 50px;
+  object-fit: cover;
+`;
+
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 16px;
+
+  p {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 17px;
+    color: #e0e0e0;
+  }
+`;
