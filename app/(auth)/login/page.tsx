e34ed234d05e0ea49d5fa6ba6e997f2a2ea0fcf8
jongjin <jongjin024@gmail.com>
2023-01-16 15:21:19 +0900

    add: auth 로 묶기

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import React from 'react';
 import { LoginPage } from 'components/pages';
 
 function Login() {
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { SignupPage } from 'components/pages';
+
+function Signup() {
+  return <SignupPage />;
+}
+
+export default Signup;
