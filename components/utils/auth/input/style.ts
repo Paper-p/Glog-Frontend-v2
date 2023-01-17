170beea87c0a8a128aca4f84ec7b80a29c77b70a
jongjin <jongjin024@gmail.com>
2023-01-17 22:11:36 +0900

    fix: margin

--- components/utils/auth/input/style.ts
+++ components/utils/auth/input/style.ts
@@ -7,7 +7,7 @@ export const InputWrapper = styled.div<{ needMargin: boolean; error: boolean }>`
   align-items: center;
   background: #252727;
   border-radius: 10px;
-  margin-bottom: ${(e) => (e.needMargin ? '1rem' : '')};
+  margin-bottom: ${(e) => (e.needMargin ? '0.675rem' : '')};
   filter: ${(e) => (e.error ? 'drop-shadow(0px 1px 7px #e45b3c)' : '')};
 `;
 
