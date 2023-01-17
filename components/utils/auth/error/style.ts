4010444509c0a5c9139e42b561b1fab32b4b0e7d
jongjin <jongjin024@gmail.com>
2023-01-17 22:12:07 +0900

    fix: error text props

--- components/utils/auth/error/style.ts
+++ components/utils/auth/error/style.ts
@@ -1,9 +1,11 @@
 import styled from '@emotion/styled';
 
-export const ErrorText = styled.p`
+export const ErrorText = styled.p<{ needMargin: boolean }>`
   font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
-  font-size: 14px;
+  font-size: 13px;
   color: #e83f28;
+  padding-left: 10px;
+  margin-bottom: ${(e) => (e.needMargin ? '0.675rem' : '')};
 `;
