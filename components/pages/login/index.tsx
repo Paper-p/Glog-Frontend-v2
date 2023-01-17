9c534f4f8ad338d1f0b2397edecdc744e818a5c2
jongjin <jongjin024@gmail.com>
2023-01-17 22:12:43 +0900

    add: login exception handling

--- components/pages/login/index.tsx
+++ components/pages/login/index.tsx
@@ -2,20 +2,61 @@ import Logo from 'components/utils/logo';
 import * as S from './style';
 import AuthInput from 'components/utils/auth/input';
 import CommonButton from 'components/common/button';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import AuthErrorText from 'components/utils/auth/error';
+
+interface LoginType {
+  userId: string;
+  password: string;
+}
 
 function LoginPage() {
+  const [valueError, setValueError] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<LoginType>();
+
+  const onValid = async (data: LoginType) => {
+    console.log('Login with data');
+  };
+
+  const inValid = (error: any) => {
+    error && setValueError(true);
+  };
+
   return (
-    <S.LoginForm>
+    <S.LoginForm onSubmit={handleSubmit(onValid, inValid)}>
       <S.LogoToCenter>
         <Logo width={335} height={96} />
       </S.LogoToCenter>
       <S.WelcomeText>다시 온걸 환영해요!</S.WelcomeText>
-      <AuthInput icon='human' needMargin={true} />
+      <AuthInput
+        icon='human'
+        error={valueError}
+        needMargin={true}
+        register={register('userId', {
+          required: '아이디는 필수입력입니다.',
+        })}
+        placeholder='아이디를 입력해주세요'
+      />
+      <AuthErrorText needMargin={true}>{errors.userId?.message}</AuthErrorText>
       <AuthInput
         icon='lock'
-        placeholder='비밀번호를 입력해주세요'
+        error={valueError}
         needMargin={true}
+        register={register('password', {
+          required: '비밀번호는 필수입력입니다.',
+        })}
+        placeholder='비밀번호를 입력해주세요'
       />
+      <AuthErrorText needMargin={true}>
+        {errors.password?.message}
+      </AuthErrorText>
       <CommonButton className='needMargin'>로그인</CommonButton>
     </S.LoginForm>
   );
