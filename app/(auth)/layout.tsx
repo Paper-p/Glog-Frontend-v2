fe29bc06fd0e5dabfad4bddcc847168319281312
jongjin <jongjin024@gmail.com>
2023-01-17 20:44:31 +0900

    fix: maximum call stack size exceeded error solved

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import AuthPageLayout from 'components/utils/auth/layout';
+import AuthLayout from 'components/utils/auth/layout';
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 function AuthPageLayout({ children }: AuthLayoutProps) {
-  return <AuthPageLayout>{children}</AuthPageLayout>;
+  return <AuthLayout>{children}</AuthLayout>;
 }
 
 export default AuthPageLayout;
