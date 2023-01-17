e7cd9a5dbd5006ea2d5456a95a6612585e1eb647
jongjin <jongjin024@gmail.com>
2023-01-17 22:43:55 +0900

    add: signup exception handling

--- components/pages/signup/index.tsx
+++ components/pages/signup/index.tsx
@@ -1,16 +1,115 @@
 import * as S from './style';
 import AuthInput from 'components/utils/auth/input';
 import CommonButton from 'components/common/button';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import AuthErrorText from 'components/utils/auth/error';
+
+interface SignupType {
+  nickname: string;
+  userId: string;
+  password: string;
+  confirmPassword: string;
+}
 
 function SignupPage() {
+  const [valueError, setValueError] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<SignupType>();
+
+  const onValid = async (data: SignupType) => {
+    console.log('Signup with data');
+  };
+
+  const inValid = (error: any) => {
+    error && setValueError(true);
+  };
+
   return (
-    <S.SignupForm>
+    <S.SignupForm onSubmit={handleSubmit(onValid, inValid)}>
       <S.Gif src='/images/Signup.gif' />
-      <AuthInput icon='human' needMargin={true} error={false} />
-      <AuthInput icon='human' needMargin={true} error={false} />
-      <AuthInput icon='lock' needMargin={true} error={false} />
-      <AuthInput icon='lock' needMargin={true} error={false} />
-      <CommonButton>회원가입</CommonButton>
+      <S.WelcomeText>당신의 시작을 환영해요!</S.WelcomeText>
+      <AuthInput
+        icon='human'
+        needMargin={true}
+        error={valueError}
+        register={register('nickname', {
+          required: '닉네임은 필수입력입니다.',
+          minLength: {
+            message: '닉네임 4자 이상이어야해요.',
+            value: 4,
+          },
+          maxLength: {
+            message: '닉네임 최대 20자 입니다.',
+            value: 20,
+          },
+        })}
+        placeholder='닉네임을 입력해주세요'
+      />
+      <AuthErrorText needMargin={true}>
+        {errors.nickname?.message}
+      </AuthErrorText>
+      <AuthInput
+        icon='human'
+        needMargin={true}
+        error={valueError}
+        register={register('userId', {
+          required: '아이디는 필수입력입니다.',
+          minLength: {
+            message: '아이디는 4자 이상이어야해요.',
+            value: 4,
+          },
+          maxLength: {
+            message: '아이디는 최대 20자 입니다.',
+            value: 20,
+          },
+        })}
+        placeholder='아이디를 입력해주세요'
+      />
+      <AuthErrorText needMargin={true}>{errors.userId?.message}</AuthErrorText>
+      <AuthInput
+        icon='lock'
+        needMargin={true}
+        error={valueError}
+        register={register('password', {
+          required: '비밀번호는 필수입력입니다.',
+          minLength: {
+            message: '비밀번호는 8자 이상이어야해요.',
+            value: 8,
+          },
+          maxLength: {
+            message: '비밀번호는 최대 20자 입니다.',
+            value: 20,
+          },
+          pattern: {
+            message: '잘못된 비밀번호 형식이에요.',
+            value:
+              /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+          },
+        })}
+        placeholder='비밀번호를 입력해주세요'
+      />
+      <AuthErrorText needMargin={true}>
+        {errors.password?.message}
+      </AuthErrorText>
+      <AuthInput
+        icon='lock'
+        needMargin={true}
+        error={valueError}
+        register={register('confirmPassword', {
+          required: '비밀번호 확인은 필수입력입니다.',
+        })}
+        placeholder='사용하실 비밀번호를 한번 더 입력해주세요.'
+      />
+      <AuthErrorText needMargin={true}>
+        {errors.confirmPassword?.message}
+      </AuthErrorText>
+      <CommonButton className='needMargin'>회원가입</CommonButton>
       <S.Reference>비밀번호는 8자리 이상 및 기호를 포함해주세요.</S.Reference>
     </S.SignupForm>
   );
