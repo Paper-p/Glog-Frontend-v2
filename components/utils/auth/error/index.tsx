c02a8290e6c55034cd2662cf94bed84bed7ab697
jongjin <jongjin024@gmail.com>
2023-01-17 22:11:54 +0900

    fix: error text props

--- components/utils/auth/error/index.tsx
+++ components/utils/auth/error/index.tsx
@@ -3,10 +3,13 @@ import * as S from './style';
 
 interface AuthErrorTextProps {
   children: React.ReactNode;
+  needMargin: boolean;
 }
 
-function AuthErrorText({ children }: AuthErrorTextProps) {
-  return <S.ErrorText>{children}</S.ErrorText>;
+function AuthErrorText(props: AuthErrorTextProps) {
+  return (
+    <S.ErrorText needMargin={props.needMargin}>{props.children}</S.ErrorText>
+  );
 }
 
 export default AuthErrorText;
