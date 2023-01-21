8f84f456d7cb33ed31bfe97240ee3c67609caa8e
jongjin <jongjin024@gmail.com>
2023-01-21 21:16:13 +0900

    add: Props

--- components/utils/post/item/normal/index.tsx
+++ components/utils/post/item/normal/index.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import * as S from './style';
 import * as I from 'assets/svg';
+import { PostData } from 'types/post.types';
 
-function NormalPostItem() {
+interface NormalItemProps {
+  list: PostData;
+}
+
+function NormalPostItem({ list }: NormalItemProps) {
   return (
-    <S.NormalPostItemLayout backgroundImage='https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt361d06c667f47dac/60dad6580401cb0ebfa74b50/dbf90410e8ce567dd5ea7b1d2e6972627d587f4b.jpg'>
+    <S.NormalPostItemLayout backgroundImage={String(list.thumbnail)}>
       <S.PaddingBox>
-        <S.PostTitle>
-          asdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsadasdsad
-        </S.PostTitle>
-        <S.PostContent>
-          asdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasd
-          asdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasdasdasdsadassdasdasd
-        </S.PostContent>
+        <S.PostTitle>{list.title} </S.PostTitle>
+        <S.PostContent>{list.previewContent}</S.PostContent>
         <S.PostStatBar>
           <S.StatWrapper>
             <I.Like />
-            <S.Stat>900</S.Stat>
+            <S.Stat>{list.likeCount}</S.Stat>
           </S.StatWrapper>
           <S.StatWrapper>
             <I.Hit />
-            <S.Stat>900</S.Stat>
+            <S.Stat>{list.hit}</S.Stat>
           </S.StatWrapper>
         </S.PostStatBar>
       </S.PaddingBox>
