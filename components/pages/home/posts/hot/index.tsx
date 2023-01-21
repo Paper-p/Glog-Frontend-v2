f341771945acbf40ffefc0390546a12779d16834
jongjin <jongjin024@gmail.com>
2023-01-21 16:23:54 +0900

    add: post preview type

--- components/pages/home/posts/hot/index.tsx
+++ components/pages/home/posts/hot/index.tsx
@@ -1,6 +1,5 @@
 import SpecialPostItem from 'components/utils/post/item/special';
 import PostTypeBoard from 'components/utils/post/typeBoard';
-import React from 'react';
 import * as S from './style';
 
 function HotPosts() {
---
+++ types/postPreview.types.ts
@@ -0,0 +1,7 @@
+export interface PostPreviewData {
+  id: number;
+  title: string;
+  previewContent: string;
+  hit: number;
+  likeCount: number;
+}
