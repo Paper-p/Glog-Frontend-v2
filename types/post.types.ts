17a2ea7bf92e05e58da78345cdef2492e04dd98f
jongjin <jongjin024@gmail.com>
2023-01-21 21:16:41 +0900

    fix: post type

--- types/post.types.ts
+++ types/post.types.ts
@@ -1,7 +1,12 @@
-export interface PostPreviewData {
+export interface PostData {
   id: number;
   title: string;
+  createdAt: Date;
+  thumbnail?: string;
   previewContent: string;
   hit: number;
   likeCount: number;
+  commentCount: number;
+  isLiked: boolean;
+  tagList: [string];
 }
