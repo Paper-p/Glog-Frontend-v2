ddc5fcfbbdb517c02acf9e7c94412ae6831c5aff
jongjin <jongjin024@gmail.com>
2023-01-21 21:16:19 +0900

    add: Props

--- components/utils/post/item/special/index.tsx
+++ components/utils/post/item/special/index.tsx
@@ -1,24 +1,27 @@
 import * as S from './style';
 import * as I from 'assets/svg';
+import { PostData } from 'types/post.types';
 
-function SpecialPostItem() {
+interface SpecialPostItemProps {
+  list: PostData;
+}
+
+function SpecialPostItem({ list }: SpecialPostItemProps) {
   return (
     <S.SpecialPostItemLayout>
-      <S.BackgroundImage src='https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt361d06c667f47dac/60dad6580401cb0ebfa74b50/dbf90410e8ce567dd5ea7b1d2e6972627d587f4b.jpg' />
+      <S.BackgroundImage src={list.thumbnail} />
       <S.InformationWrapper>
         <S.PostInformation>
-          <S.PostTitle>유저리서치(UserResearch)란?</S.PostTitle>
-          <S.PostContent>
-            유저리서치(UserResearch)란?유저리서치(UserResearch)란?유저리서치(UserResearch)란?유저리서치(UserResearch)란?유저리서치(UserResearch)란?유저리서치(UserResearch)란?유저리서치(UserResearch)란?유저리서치(UserResearch)란?
-          </S.PostContent>
+          <S.PostTitle>{list.title}</S.PostTitle>
+          <S.PostContent>{list.previewContent}</S.PostContent>
           <S.PostStatBar>
             <S.StatWrapper>
               <I.Like />
-              <S.Stat>900</S.Stat>
+              <S.Stat>{list.likeCount}</S.Stat>
             </S.StatWrapper>
             <S.StatWrapper>
               <I.Hit />
-              <S.Stat>900</S.Stat>
+              <S.Stat>{list.hit}</S.Stat>
             </S.StatWrapper>
           </S.PostStatBar>
         </S.PostInformation>
