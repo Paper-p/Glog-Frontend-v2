7f2441b01c3a05e56de5e66f3fc3b888890fbb86
jongjin <jongjin024@gmail.com>
2023-01-21 18:40:49 +0900

    fix: normal posts grid gap

--- components/pages/home/posts/normal/style.ts
+++ components/pages/home/posts/normal/style.ts
@@ -4,7 +4,7 @@ export const NormalPostsLayout = styled.section`
   width: 100%;
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
-  gap: 24px;
+  gap: 16px;
 
   @media screen and (max-width: 900px) {
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
