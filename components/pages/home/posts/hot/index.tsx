136a76bfe9e8aae0184133e28f0c500c77cf9b27
jongjin <jongjin024@gmail.com>
2023-01-21 21:15:55 +0900

    add: getHotPostsList

--- components/pages/home/posts/hot/index.tsx
+++ components/pages/home/posts/hot/index.tsx
@@ -1,16 +1,34 @@
 import SpecialPostItem from 'components/utils/post/item/special';
 import PostTypeBoard from 'components/utils/post/typeBoard';
+import feed from 'network/request/feed';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import * as S from './style';
 
 function HotPosts() {
+  const [list, setList] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getHotPosts = async () => {
+      try {
+        const response: any = await feed.getHotPostsList();
+        setList(response.data.list);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getHotPosts();
+  }, []);
   return (
     <>
       <PostTypeBoard>🔥HOT’</PostTypeBoard>
       <S.HotPostsLayout>
-        <SpecialPostItem />
-        <SpecialPostItem />
-        <SpecialPostItem />
-        <SpecialPostItem />
+        {list.map((idx) => (
+          <React.Fragment key={idx.id}>
+            <SpecialPostItem list={idx} />
+          </React.Fragment>
+        ))}
       </S.HotPostsLayout>
     </>
   );
