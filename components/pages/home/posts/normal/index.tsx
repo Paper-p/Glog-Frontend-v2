1b81425d49a5e1a3e9ad764b390702b8cda98221
jongjin <jongjin024@gmail.com>
2023-01-21 21:16:04 +0900

    add: getNormalPostsList

--- components/pages/home/posts/normal/index.tsx
+++ components/pages/home/posts/normal/index.tsx
@@ -1,15 +1,37 @@
 import NormalPostItem from 'components/utils/post/item/normal';
 import * as S from './style';
 import PostTypeBoard from 'components/utils/post/typeBoard';
+import feed from 'network/request/feed';
+import { useState, useEffect } from 'react';
+import React from 'react';
 
 function NormalPosts() {
+  const [list, setList] = useState<any[]>([]);
+
+  useEffect(() => {
+    const getHotPosts = async () => {
+      try {
+        const response: any = await feed.getNormalPostsList({
+          page: 0,
+          size: 5,
+        });
+        setList(response.data.list);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getHotPosts();
+  }, []);
   return (
     <>
       <PostTypeBoard>💻 게시물’s</PostTypeBoard>
       <S.NormalPostsLayout>
-        <NormalPostItem />
-        <NormalPostItem />
-        <NormalPostItem />
+        {list.map((idx) => (
+          <React.Fragment key={idx.id}>
+            <NormalPostItem list={idx} />
+          </React.Fragment>
+        ))}
       </S.NormalPostsLayout>
     </>
   );
