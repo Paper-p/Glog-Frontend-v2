d6d34e04c771fbda817a3d3525d08bd11e55a8de
jongjin <jongjin024@gmail.com>
2023-01-21 22:40:58 +0900

    add: window in token service

--- utils/tokenService.ts
+++ utils/tokenService.ts
@@ -2,26 +2,26 @@ import { TokenData } from 'types/token.types';
 
 class TokenService {
   getLocalRefreshToken() {
-    const user = JSON.parse(localStorage.getItem('token') || '{}');
+    const user = JSON.parse(window.localStorage.getItem('token') || '{}');
     return user?.refreshToken;
   }
   getLocalAccessToken() {
-    const user = JSON.parse(localStorage.getItem('token') || '{}');
+    const user = JSON.parse(window.localStorage.getItem('token') || '{}');
     return user?.accessToken;
   }
   updateLocalAccessToken(token: string) {
-    const user = JSON.parse(localStorage.getItem('token') || '{}');
+    const user = JSON.parse(window.localStorage.getItem('token') || '{}');
     user.accessToken = token;
-    localStorage.setItem('token', JSON.stringify(user));
+    window.localStorage.setItem('token', JSON.stringify(user));
   }
   getUser() {
-    return JSON.parse(localStorage.getItem('token') || '{}');
+    return JSON.parse(window.localStorage.getItem('token') || '{}');
   }
   setUser(user: TokenData) {
-    localStorage.setItem('token', JSON.stringify(user));
+    window.localStorage.setItem('token', JSON.stringify(user));
   }
   removeUser() {
-    localStorage.removeItem('token');
+    window.localStorage.removeItem('token');
   }
 }
 
