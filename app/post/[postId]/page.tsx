9f5c26a2788851f6e6e8e67fc2d9b4b7e94d450d
jongjin <jongjin024@gmail.com>
2023-01-22 21:35:29 +0900

    add: create post page

---
+++ app/post/[postId]/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { PostPage } from 'components/pages';
+
+function Post() {
+  return <PostPage />;
+}
+
+export default Post;
---
+++ components/pages/post/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+function PostPage() {
+  return <S.Layout></S.Layout>;
+}
+
+export default PostPage;
