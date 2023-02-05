9f6722ac7d53ae74ad10e84243cbbc4144371306
jongjin <jongjin024@gmail.com>
2023-02-06 03:55:56 +0900

    add: post page publishing

--- components/pages/post/index.tsx
+++ components/pages/post/index.tsx
@@ -1,7 +1,39 @@
 import * as S from './style';
+import * as I from 'assets/svg';
+import Image from 'next/image';
 
 function PostPage() {
-  return <S.Layout></S.Layout>;
+  return (
+    <S.PostPageLayout>
+      <S.PostTitle>asd</S.PostTitle>
+      <S.TagList>
+        <S.Tag>asd</S.Tag>
+        <S.Tag>asd</S.Tag>
+        <S.Tag>asd</S.Tag>
+        <S.Tag>asd</S.Tag>
+      </S.TagList>
+      <S.PostInfoBox>
+        <S.Author>
+          <S.ProfileImage src='https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(1208x347:1210x345)/origin-imgresizer.eurosport.com/2023/01/12/3524154-71830248-2560-1440.jpg' />
+          <S.Wrapper>
+            <S.Name>오종진</S.Name>
+            <S.DatePostCreated>222222</S.DatePostCreated>
+          </S.Wrapper>
+        </S.Author>
+        <S.PostStatBar>
+          <S.StatWrapper>
+            <I.Like />
+            <S.Stat>203</S.Stat>
+          </S.StatWrapper>
+          <S.StatWrapper>
+            <I.Hit />
+            <S.Stat>215</S.Stat>
+          </S.StatWrapper>
+        </S.PostStatBar>
+      </S.PostInfoBox>
+      <S.Thumbnail src='https://imgresizer.eurosport.com/unsafe/1200x0/filters:format(jpeg):focal(1208x347:1210x345)/origin-imgresizer.eurosport.com/2023/01/12/3524154-71830248-2560-1440.jpg' />
+    </S.PostPageLayout>
+  );
 }
 
 export default PostPage;
--- components/pages/post/style.ts
+++ components/pages/post/style.ts
@@ -1,3 +1,106 @@
 import styled from '@emotion/styled';
 
-export const Layout = styled.section``;
+export const PostPageLayout = styled.section`
+  width: 60%;
+  margin: 6rem auto 0;
+`;
+
+export const PostTitle = styled.p`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 3em;
+  color: #ffffff;
+`;
+
+export const TagList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  height: content;
+  display: flex;
+  gap: 10px 6px;
+  margin-top: 1.8rem;
+`;
+
+export const Tag = styled.div`
+  word-break: break-all;
+  height: content;
+  padding: 10px 18px;
+  background: #3a3b3d;
+  border-radius: 10px;
+  font-size: 0.88em;
+  font-weight: bold;
+  color: #969696;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export const PostInfoBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 1.6rem;
+`;
+
+export const Author = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 8px;
+  align-items: center;
+`;
+
+export const ProfileImage = styled.img`
+  width: 37px;
+  height: 37px;
+  object-fit: cover;
+  border-radius: 40px;
+`;
+
+export const Wrapper = styled.div``;
+
+export const Name = styled.p`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  color: #ffffff;
+`;
+
+export const DatePostCreated = styled.p`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  color: #999999;
+`;
+
+export const PostStatBar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const StatWrapper = styled.div`
+  display: flex;
+  gap: 4px;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Stat = styled.p`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 12px;
+  color: #929292;
+  margin: 0.1rem 0 0 0;
+`;
+
+export const Thumbnail = styled.img`
+  width: 100%;
+  margin-top: 3rem;
+`;
