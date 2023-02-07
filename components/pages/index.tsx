4782aab06ef1d2b7d3b622f2f8e411be9ce40286
jongjin <jongjin024@gmail.com>
2023-02-07 19:16:32 +0900

    fix: folder structure

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -1,4 +1,4 @@
-export { default as LoginPage } from './login';
-export { default as SignupPage } from './signup';
+export { default as LoginPage } from './auth/login';
+export { default as SignupPage } from './auth/signup';
 export { default as HomePage } from './home/page';
 export { default as ReadPostPage } from './post/read/page';
