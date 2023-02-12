96948c6d987efbeacb49a9f8facdd69f878b7f0c
jongjin <jongjin024@gmail.com>
2023-02-12 17:45:16 +0900

    add: setting query client in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,20 +4,25 @@ import Header from 'components/common/header';
 import GlobalStyle from 'styles/global';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 function RootLayout({ children }: RootLayoutProps) {
+  const queryClient = new QueryClient();
+
   return (
     <html lang='ko'>
       <head />
       <body>
-        <ToastContainer />
-        <GlobalStyle />
-        <Header />
-        {children}
+        <QueryClientProvider client={queryClient}>
+          <ToastContainer />
+          <GlobalStyle />
+          <Header />
+          {children}
+        </QueryClientProvider>
       </body>
     </html>
   );
