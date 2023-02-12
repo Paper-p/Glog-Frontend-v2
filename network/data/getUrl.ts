ab859015a1d27a947e9a00071706ff978c43c6e2
jongjin <jongjin024@gmail.com>
2023-02-12 18:31:56 +0900

    add: create comment api

--- network/data/getUrl.ts
+++ network/data/getUrl.ts
@@ -39,3 +39,9 @@ export const userUrl = {
     return 'user/profile';
   },
 };
+
+export const commentUrl = {
+  getCommentUrl: () => {
+    return 'comment/';
+  },
+};
---
+++ network/request/comment.ts
@@ -0,0 +1,24 @@
+import { commentUrl } from 'network/data/getUrl';
+import { instance } from 'network/data/instance';
+import tokenService from 'utils/tokenService';
+
+class Comment {
+  createComment(postId: string, comment: string) {
+    try {
+      return instance({
+        method: 'POST',
+        url: commentUrl.getCommentUrl() + postId,
+        headers: {
+          Authorization: 'Bearer ' + tokenService.getLocalAccessToken(),
+        },
+        data: {
+          content: comment,
+        },
+      });
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
+export default new Comment();
