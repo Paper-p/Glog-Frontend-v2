0dcee982cf7798a6ffd954f83b49cf12976eb6f4
jongjin <jongjin024@gmail.com>
2023-02-17 20:43:25 +0900

    fix: undefined check

--- components/utils/post/item/ui/information/index.tsx
+++ components/utils/post/item/ui/information/index.tsx
@@ -9,16 +9,16 @@ interface ItemInformationProps {
 function PostItemInformation({ data }: ItemInformationProps) {
   return (
     <>
-      <S.PostTitle>{data.title}</S.PostTitle>
-      <S.PostContent>{data.previewContent}</S.PostContent>
+      <S.PostTitle>{data?.title}</S.PostTitle>
+      <S.PostContent>{data?.previewContent}</S.PostContent>
       <S.PostStatBar>
         <S.StatWrapper>
           <I.Like />
-          <S.Stat>{data.likeCount}</S.Stat>
+          <S.Stat>{data?.likeCount}</S.Stat>
         </S.StatWrapper>
         <S.StatWrapper>
           <I.Hit />
-          <S.Stat>{data.hit}</S.Stat>
+          <S.Stat>{data?.hit}</S.Stat>
         </S.StatWrapper>
       </S.PostStatBar>
     </>
