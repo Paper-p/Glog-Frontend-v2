e1b76e138ccfd4305a2309130f0f65f6ee4fbedb
jongjin <jongjin024@gmail.com>
2023-02-19 16:39:22 +0900

    remove: legacy

--- components/pages/user/page/style.ts
+++ components/pages/user/page/style.ts
@@ -6,20 +6,3 @@ export const UserPostsSection = styled.section`
   width: 72.5vw;
   margin: auto;
 `;
-
-export const SignBoardBox = styled.div`
-  display: flex;
-  gap: 10px;
-  margin-top: 25px;
-`;
-
-export const PostList = styled.div`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
-  gap: 16px;
-
-  @media screen and (max-width: 900px) {
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  }
-`;
