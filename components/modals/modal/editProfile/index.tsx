2f9b87b527f8638eeb33304b81c7e21206e350f0
jongjin <jongjin024@gmail.com>
2023-02-19 19:15:07 +0900

    add: EditProfileModal

---
+++ components/modals/modal/editProfile/index.tsx
@@ -0,0 +1,59 @@
+import * as S from './style';
+import * as I from 'assets/svg';
+import ModalLayout from 'components/modals/layout';
+import { modalsAtomFamily } from 'atoms';
+import error from 'next/error';
+import { useRecoilState } from 'recoil';
+import { useState } from 'react';
+
+function EditProfileModal() {
+  const [_, setEditProfileModal] = useRecoilState(
+    modalsAtomFamily('editProfileModal')
+  );
+  const [loading, setLoading] = useState<boolean>(false);
+
+  return (
+    <ModalLayout setModal={setEditProfileModal}>
+      <S.EditProfileModalLayout onClick={(e) => e.stopPropagation()}>
+        <S.ProfileBox>
+          <S.ImageBox>
+            <S.ImageChangeBox>
+              <S.EditSvg>
+                <I.EditIcon />
+              </S.EditSvg>
+              <p>변경</p>
+            </S.ImageChangeBox>
+            {loading ? (
+              <S.ProfileImage src='/images/loading.gif' />
+            ) : (
+              <S.ProfileImage src={'profileImage'} />
+            )}
+            <input
+              // ref={setImage}
+              type={'file'}
+              id={'profile'}
+              accept={'image/*'}
+              name={'file'}
+              // onChange={imgHandler}
+            />
+          </S.ImageBox>
+          <S.Nickname
+            placeholder='닉네임을 입력하세요'
+            // value={name}
+            // onChange={onChange}
+            name='name'
+            error={'error'}
+          />
+        </S.ProfileBox>
+        {error && <S.ErrorMessage>{'error'}</S.ErrorMessage>}
+        {/* <S.MoveBox error={error}> */}
+        <S.MoveBox error={''}>
+          <S.Exit>취소하기</S.Exit>
+          <S.Edit>수정완료</S.Edit>
+        </S.MoveBox>
+      </S.EditProfileModalLayout>
+    </ModalLayout>
+  );
+}
+
+export default EditProfileModal;
