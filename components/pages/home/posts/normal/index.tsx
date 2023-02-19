4aaf8910983e5d71083290aea9638fd79dd39231
jongjin <jongjin024@gmail.com>
2023-02-19 18:28:56 +0900

    add: main page infinite scroll feature

--- components/pages/home/posts/normal/index.tsx
+++ components/pages/home/posts/normal/index.tsx
@@ -1,28 +1,46 @@
 import * as S from './style';
 import SignBoard from 'components/common/signBoard';
 import feed from 'network/request/feed';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import PostItem from 'components/utils/post/item';
 
 function NormalPosts() {
+  const page = useRef<number>(0);
+  const [hasNextPage, setHasNextPage] = useState<boolean>(true);
+  const observerTargetEl = useRef<HTMLDivElement>(null);
+  const [isLoad, setIsLoad] = useState<boolean>(false);
   const [list, setList] = useState<any[]>([]);
 
-  useEffect(() => {
-    const getNormalPosts = async () => {
-      try {
-        const response: any = await feed.getNormalPostsList({
-          page: 0,
-          size: 5,
-        });
-        setList(response.data.list);
-      } catch (e) {
-        console.log(e);
+  const getNormalPosts = async () => {
+    try {
+      const response: any = await feed.getNormalPostsList({
+        page: page.current,
+        size: 6,
+      });
+      setList((prevPosts) => [...prevPosts, ...response.data.list]);
+      setHasNextPage(response.data.list.length === 6);
+      setIsLoad(false);
+      if (response.data.list.length) {
+        page.current += 1;
       }
-    };
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-    getNormalPosts();
-  }, []);
+  useEffect(() => {
+    if (!observerTargetEl.current || !hasNextPage) return;
+
+    const io = new IntersectionObserver((entries, observer) => {
+      if (entries[0].isIntersecting && !isLoad) {
+        getNormalPosts();
+      }
+    });
+
+    io.observe(observerTargetEl.current);
+    return () => io.disconnect();
+  }, [hasNextPage, getNormalPosts, isLoad]);
   return (
     <>
       <SignBoard>💻 게시물’s</SignBoard>
@@ -32,6 +50,7 @@ function NormalPosts() {
             <PostItem shape='rectangle' data={currentValue} />
           </Link>
         ))}
+        <div ref={observerTargetEl} />
       </S.NormalPostsLayout>
     </>
   );
