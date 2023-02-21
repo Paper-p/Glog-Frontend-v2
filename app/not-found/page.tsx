5163e26df0b8dacaa1a13b2a8ce495e2a67ba2dd
jongjin <jongjin024@gmail.com>
2023-02-21 16:38:40 +0900

    add: not-found page

---
+++ app/not-found/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import NotFoundPage from 'components/pages/notFound';
+import { useSearchParams } from 'next/navigation';
+
+function NotFound() {
+  const searchParams = useSearchParams();
+  const type404 = ['유저', '게시물'];
+
+  if (type404.includes(String(searchParams.get('type')))) {
+    return <NotFoundPage notFoundType={searchParams.get('type')} />;
+  } else {
+    return <NotFoundPage notFoundType={'페이지'} />;
+  }
+}
+
+export default NotFound;
---
+++ components/pages/notFound/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './style';
+
+function NotFoundPage({ notFoundType }: { notFoundType: string | null }) {
+  return (
+    <S.NotFoundSectionLayout>
+      <S.MainText>404 Not Found</S.MainText>
+      <S.SubText>{notFoundType}를 찾을 수 없어요.</S.SubText>
+    </S.NotFoundSectionLayout>
+  );
+}
+
+export default NotFoundPage;
