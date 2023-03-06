22ae14f572a2979c1dd2489a463e3dbab5fd9428
jongjin <jongjin024@gmail.com>
2023-03-06 11:05:03 +0900

    add: hot posts skeleton ui

---
+++ components/utils/skeleton/hot/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+function HotPostsSkeleton() {
+  return <S.HotPostsSkeletonItem />;
+}
+
+export default HotPostsSkeleton;
---
+++ components/utils/skeleton/hot/style.ts
@@ -0,0 +1,30 @@
+import styled from '@emotion/styled';
+
+export const HotPostsSkeletonItem = styled.div`
+  @-webkit-keyframes skeleton-gradient {
+    0% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+    50% {
+      background-color: rgba(165, 165, 165, 0.3);
+    }
+    100% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+  }
+  @keyframes skeleton-gradient {
+    0% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+    50% {
+      background-color: rgba(165, 165, 165, 0.3);
+    }
+    100% {
+      background-color: rgba(165, 165, 165, 0.1);
+    }
+  }
+  -webkit-animation: skeleton-gradient 1.5s infinite ease-in-out;
+  animation: skeleton-gradient 1.5s infinite ease-in-out;
+  height: 330px;
+  border-radius: 10px;
+`;
