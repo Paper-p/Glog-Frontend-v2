b7525f143919cf8704d65f7334d154b1178a382a
jongjin <jongjin024@gmail.com>
2023-03-13 17:03:56 +0900

    add: 검색 시 Hotposts 제거

--- components/pages/home/page/index.tsx
+++ components/pages/home/page/index.tsx
@@ -1,11 +1,15 @@
+import { searchAtom } from 'atoms';
+import { useRecoilState } from 'recoil';
 import HotPosts from '../posts/hot';
 import NormalPosts from '../posts/normal';
 import * as S from './style';
 
 function HomePage() {
+  const [search] = useRecoilState(searchAtom);
+
   return (
     <S.HomePageLayout>
-      <HotPosts />
+      {!search.keyword && <HotPosts />}
       <NormalPosts />
     </S.HomePageLayout>
   );
