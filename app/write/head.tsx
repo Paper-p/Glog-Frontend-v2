e146752394e4df48bb608829ec9a16032d5e4844
jongjin1010 <jongjin024@gmail.com>
2023-04-17 20:58:38 +0900

    add: Head

---
+++ app/write/head.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+function Head() {
+  return (
+    <>
+      <title>Glog | 글쓰기</title>
+      <meta name='description' content={'커뮤니티에 글을 출판하세요'} />
+      <meta name='og:description' content={'커뮤니티에 글을 출판하세요'} />
+    </>
+  );
+}
+
+export default Head;
