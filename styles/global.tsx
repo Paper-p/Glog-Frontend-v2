4a2b3170747c938cd04006024177ca69682bbd6e
오종진 <jongjin024@gmail.com>
2023-08-30 18:26:57 +0900

    fix: font type

--- styles/global.tsx
+++ styles/global.tsx
@@ -7,9 +7,38 @@ function GlobalStyle() {
       styles={css`
         ${emotionReset}
 
+        @font-face {
+          font-family: 'AppleSDGothicNeoB00';
+          font-style: normal;
+          src: url('/font/AppleSDGothicNeoB.woff') format('woff'),
+            url('/font/AppleSDGothicNeoB.woff2') format('font-woff2');
+        }
+
+        @font-face {
+          font-family: 'AppleSDGothicNeoM00';
+          font-style: normal;
+          src: url('/font/AppleSDGothicNeoM.woff') format('woff'),
+            url('/font/AppleSDGothicNeoM.woff2') format('font-woff2');
+        }
+
+        @font-face {
+          font-family: 'AppleSDGothicNeoEB00';
+          font-style: normal;
+          src: url('/font/AppleSDGothicNeoEB.woff') format('woff');
+        }
+        @font-face {
+          font-family: 'AppleSDGothicNeoSB00';
+          font-style: normal;
+          src: url('../font/AppleSDGothicNeoSB.woff') format('woff');
+        }
+
         body {
-          font-family: 'Noto Sans KR', sans-serif;
+          font-family: 'AppleSDGothicNeoM00', sans-serif;
           background-color: #1a1b1e;
+        }
+
+        p {
+          font-family: 'AppleSDGothicNeoM00', sans-serif !important;
         }
 
         a {
@@ -18,11 +47,11 @@ function GlobalStyle() {
         }
 
         button {
-          font-family: 'Noto Sans KR', sans-serif;
+          font-family: 'AppleSDGothicNeoM00', sans-serif;
         }
 
         input {
-          font-family: 'Noto Sans KR', sans-serif;
+          font-family: 'AppleSDGothicNeoM00', sans-serif;
         }
       `}
     />
