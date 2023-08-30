e2f615c65bbf260eca5d0632b92e971d839cd584
오종진 <jongjin024@gmail.com>
2023-08-30 23:12:00 +0900

    add: eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,5 +19,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/no-unknown-property': ['error', { ignore: ['css'] }],
+    '@typescript-eslint/no-explicit-any': ['off'],
   },
 };
