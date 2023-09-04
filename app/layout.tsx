ca8d6e57f61376c6ec2fc65ad153b3aea953efb8
오종진 <jongjin024@gmail.com>
2023-09-04 13:36:35 +0900

    add: Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,6 @@
-'use client';
-
-import GlobalStyle from 'styles/global';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
 import dynamic from 'next/dynamic';
+import Providers from './Providers';
 
 const Header = dynamic(() => import('components/common/header'), {
   ssr: false,
@@ -16,20 +11,11 @@ interface RootLayoutProps {
 }
 
 function RootLayout({ children }: RootLayoutProps) {
-  const queryClient = new QueryClient();
-
   return (
     <html lang='ko'>
       <head />
       <body>
-        <QueryClientProvider client={queryClient}>
-          <RecoilRoot>
-            <ToastContainer />
-            <GlobalStyle />
-            <Header />
-            {children}
-          </RecoilRoot>
-        </QueryClientProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
